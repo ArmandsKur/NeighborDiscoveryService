b8fc4108aac9f8d5fb7f6d1409b93c8174a3159f
Armands Kurevskis <armandskur@gmail.com>
2025-12-22 22:13:41 +0200

    Init commit

---
+++ cmake-build-debug/CMakeFiles/NeighborDiscoveryService.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NeighborDiscoveryService.dir/main.cpp.o"
+  "CMakeFiles/NeighborDiscoveryService.dir/main.cpp.o.d"
+  "NeighborDiscoveryService"
+  "NeighborDiscoveryService.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NeighborDiscoveryService.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/NeighborDiscoveryService.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for NeighborDiscoveryService.
+# This may be replaced when dependencies are built.
